4043d67ce45d94121eebe3ff0443d88ce2a83ce7
Ju <jufarias13@gmail.com>
2024-04-21 19:13:20 -0300

    adição do contrato

--- exercicio_aula_18-04_lista-3-numeros.rkt
+++ exercicio_aula_18-04_lista-3-numeros.rkt
@@ -1,7 +1,18 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercicio_aula_18-04_lista-3-numeros) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; uma lista-3-numeros é
+;; (cons n l3n), onde
+;; n: numero
+;; l3n: lista-3-numeros
+
 (define lista-3-numeros (cons 1 (cons 2 (cons 3 empty))))
+
+;; soma-3: lista -> numero
+;; Obj: dado uma lista de três números, devolver a soma dos números
+;; Exemplos:
+;; (soma-3 (cons 1 (cons 1 (cons 1 empty)))) = 3
+;; (soma-3 lista-3-numeros) = 6
 
 (define (soma-3 lista)
   (+
@@ -10,3 +21,7 @@
    (first (rest (rest lista)))
   )
 )
+
+;; Testes
+(check-expect (soma-3 (cons 1 (cons 1 (cons 1 empty)))) 3)
+(check-expect (soma-3 lista-3-numeros) 6)
